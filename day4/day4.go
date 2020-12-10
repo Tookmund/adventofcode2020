731e3cea9519efdcc659bb5460b445256823db9c
Jacob Adams <jacob@tookmund.com>
2020-12-10 15:38:31 -0500

    day 4 part 2

--- day4/day4.go
+++ day4/day4.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -28,15 +30,73 @@ func main() {
 	}
 
 	required := []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+	hgtreg := regexp.MustCompile(`^(\d+)(cm|in)$`)
+	hclreg := regexp.MustCompile(`^#[0-9a-f]{6}$`)
+	eclreg := regexp.MustCompile(`^(amb|blu|brn|gry|grn|hzl|oth)$`)
+	pidreg := regexp.MustCompile(`^\d{9}$`)
 	valid := 0
 	for _, pass := range passports {
 		numreq := 0
+		numvalid := 0
+
 		for _, req := range required {
-			if _, ok := pass[req]; ok  {
+			if v, ok := pass[req]; ok {
 				numreq++
+				switch req {
+				case "byr":
+					iv, err := strconv.Atoi(v)
+					if err != nil {
+						continue
+					}
+					if len(v) == 4 && iv >= 1920 && iv <= 2002 {
+						numvalid++
+					}
+				case "iyr":
+					iv, err := strconv.Atoi(v)
+					if err != nil {
+						continue
+					}
+					if len(v) == 4 && iv >= 2010 && iv <= 2020 {
+						numvalid++
+					}
+				case "eyr":
+					iv, err := strconv.Atoi(v)
+					if err != nil {
+						continue
+					}
+					if len(v) == 4 && iv >= 2020 && iv <= 2030 {
+						numvalid++
+					}
+				case "hgt":
+					h := hgtreg.FindStringSubmatch(v)
+					if h != nil {
+						unit := string(h[2])
+						size, err := strconv.Atoi(h[1])
+						if err != nil {
+							continue
+						}
+						if unit == "cm" && size >= 150 && size <= 193 {
+							numvalid++
+						} else if unit == "in" && size >= 59 && size <= 76 {
+							numvalid++
+						}
+					}
+				case "hcl":
+					if hclreg.MatchString(v) {
+						numvalid++
+					}
+				case "ecl":
+					if eclreg.MatchString(v) {
+						numvalid++
+					}
+				case "pid":
+					if pidreg.MatchString(v) {
+						numvalid++
+					}
+				}
 			}
 		}
-		if numreq == len(required) {
+		if numreq == len(required) && numvalid == len(required) {
 			valid++
 		}
 	}
