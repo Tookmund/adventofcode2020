ea9e93e677c19651feaba90745d00e67bf6cdf93
Jacob Adams <jacob@tookmund.com>
2020-12-10 17:17:09 -0500

    day 8 part 2

--- day8/day8.go
+++ day8/day8.go
@@ -1,36 +1,67 @@
 package main
+
 import (
 	"bufio"
-	"strings"
-	"strconv"
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func main () {
+type program_t [][]string
+
+func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	program := make([]string, 0)
-	hasrun := make([]bool, 0)
+	program := make(program_t, 0)
 	for scanner.Scan() {
-		program = append(program, scanner.Text())
-		hasrun = append(hasrun, false)
+		program = append(program, strings.Split(scanner.Text(), " "))
 	}
 	if scerr := scanner.Err(); scerr != nil {
 		panic(scerr)
 	}
+	for i := range program {
+		if program[i][0] == "nop" {
+			program[i][0] = "jmp"
+			if acc, err := runProgram(program); err == nil {
+				fmt.Println("Changed", i+1, "to JMP")
+				fmt.Println("ACC:", acc)
+				break
+			}
+			program[i][0] = "nop"
+		} else if program[i][0] == "jmp" {
+			program[i][0] = "nop"
+			if acc, err := runProgram(program); err == nil {
+				fmt.Println("Changed", i+1, "to NOP")
+				fmt.Println("ACC:", acc)
+				break
+			}
+			program[i][0] = "jmp"
+		}
+	}
+}
+
+func runProgram(program program_t) (int, error) {
+	hasrun := make([]bool, len(program))
 	acc := 0
 	pc := 0
+	reterr := errors.New("Does Not Terminate")
 	for {
+		if pc == len(program) {
+			fmt.Println("SUCCESS")
+			reterr = nil
+			break
+		}
+		fmt.Println(pc, program[pc])
 		if hasrun[pc] {
 			break
 		}
 		hasrun[pc] = true
-		line := strings.Split(program[pc], " ")
-		i, err := strconv.Atoi(line[1])
+		i, err := strconv.Atoi(program[pc][1])
 		if err != nil {
 			panic(err)
 		}
-		switch line[0] {
+		switch program[pc][0] {
 		case "nop":
 			// Nothing
 		case "acc":
@@ -41,5 +72,5 @@ func main () {
 		}
 		pc++
 	}
-	fmt.Println(acc)
+	return acc, reterr
 }
