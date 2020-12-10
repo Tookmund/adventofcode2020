3437deada6cf5f3ce2a42c4c071dc6b555d865c7
Jacob Adams <jacob@tookmund.com>
2020-12-10 14:24:27 -0500

    day 3 part 2

--- day3/day3.go
+++ day3/day3.go
@@ -15,18 +15,26 @@ func main() {
 	if scerr := scanner.Err(); scerr != nil {
 		panic(scerr)
 	}
-	v := 0
-	dv := 1
-	h := 0
-	dh := 3
 
-	numtrees := 0
-	for v < len(trees) {
-		if trees[v][h%len(trees[v])] == '#' {
-			numtrees++
+	slopes := [][]int{[]int{1, 1}, []int{3, 1}, []int{5, 1}, []int{7, 1}, []int{1, 2}}
+
+	multtrees := 1
+
+	for i := range slopes {
+		v := 0
+		dv := slopes[i][1]
+		h := 0
+		dh := slopes[i][0]
+
+		numtrees := 0
+		for v < len(trees) {
+			if trees[v][h%len(trees[v])] == '#' {
+				numtrees++
+			}
+			v += dv
+			h += dh
 		}
-		v += dv
-		h += dh
+		multtrees *= numtrees
 	}
-	fmt.Println(numtrees)
+	fmt.Println(multtrees)
 }
