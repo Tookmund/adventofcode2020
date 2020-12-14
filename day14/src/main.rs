887874408e1d29de165ffc8ccd76cbd12d86c1a5
Jacob Adams <jacob@tookmund.com>
2020-12-14 15:19:31 -0500

    day 14 part 2
    Grossly recursive and memory-wasteful

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -2,9 +2,10 @@ use std::io;
 use std::io::prelude::*;
 use std::collections::HashMap;
 use std::fmt;
+use std::str;
 
 fn main() -> io::Result<()> {
-    let mut mem = HashMap::new();
+    let mut mem: HashMap<u64, u64> = HashMap::new();
     let mut mask = String::from("UNSET");
     for line in io::stdin().lock().lines() {
         let l = line?;
@@ -15,13 +16,12 @@ fn main() -> io::Result<()> {
             let newmem = strtoint(&split[0][4..split[0].len()-1]);
             let newnum = strtoint(split[2]);
             let newbin = binform(newnum);
-            println!("Mem: {}", newmem);
+            let newmembin = binform(newmem);
+            println!("Mem:\t{}\t(decimal {})", newmembin, newmem);
             println!("value:\t{}\t(decimal {})", newbin, newnum);
             println!("mask:\t{}", mask);
-            let resultbin = getresultnum(&mask, &newbin);
-            let resultnum = intform(&resultbin);
-            println!("result:\t{}\t(decimal {})\n", resultbin, resultnum);
-            mem.insert(newmem, resultnum);
+            let mut mb = mask.as_bytes().to_owned();
+            part2(0, &mut mem, &mut mb, &newmembin.as_bytes(), &newnum);
         }
     }
     let mut total = 0;
@@ -47,7 +47,7 @@ fn intform(b: &str) -> u64 {
     u64::from_str_radix(b, 2).unwrap()
 }
 
-fn getresultnum(mask: &str, newnum: &str) -> String {
+fn _part1(mask: &str, newnum: &str) -> String {
     let mut ret: String = "".to_owned();
     let maskb = mask.as_bytes();
     for i in 0..maskb.len() {
@@ -58,4 +58,26 @@ fn getresultnum(mask: &str, newnum: &str) -> String {
         }
     }
     return ret;
+}
+
+fn part2(curmaski: usize, mem: &mut HashMap<u64, u64>, mask: &mut [u8],
+         addr: &[u8], val: &u64) {
+    let mut writeaddr = addr.to_owned();
+    for i in curmaski..mask.len() {
+        if mask[i] == b'1' {
+            writeaddr[i] = b'1';
+        } else if mask[i] == b'X' {
+            writeaddr[i] = b'0';
+            part2(i+1, mem, mask, &writeaddr, val);
+            writeaddr[i] = b'1';
+            part2(i+1, mem, mask, &writeaddr, val);
+        }
+    }
+    let wstr = match str::from_utf8(&writeaddr) {
+        Ok(v) => v,
+        Err(e) => panic!("Failed to parse ({:?}) into str: {}", writeaddr, e),
+    };
+    let wint = intform(&wstr);
+    println!("{}\t(decimal {}) = {}", wstr, wint, *val);
+    mem.insert(wint, *val);
 }
