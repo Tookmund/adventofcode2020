7f68400a2eb6cfc303e9ec8b7eb438ecfed45427
Jacob Adams <jacob@tookmund.com>
2020-12-13 19:06:58 -0500

    day 7 part 2

--- day7/day7.go
+++ day7/day7.go
@@ -14,10 +14,12 @@ type Bag struct {
 	count int
 }
 
+type BagMap map[string][]Bag
+
 func main() {
 	bagre := regexp.MustCompile(`(\d+) (\w+ \w+) bag(:?s)*`)
 	scanner := bufio.NewScanner(os.Stdin)
-	bags := make(map[string][]Bag, 0)
+	bags := make(BagMap, 0)
 	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), "contain")
 		line[1] = strings.TrimSuffix(line[1], ".")
@@ -27,30 +29,48 @@ func main() {
 		contents[0] = strings.TrimSpace(contents[0])
 		if contents[0] == "no other bags" {
 			bags[bag] = nil
-			continue
-		}
-		bagcont := make([]Bag, 0, 2)
-		for _, c := range contents {
-			fmt.Println(bag, c)
-			curbag := Bag{"", 0}
-			m := bagre.FindStringSubmatch(c)
-			var err error
-			curbag.count, err = strconv.Atoi(m[1])
-			if err != nil {
-				panic(err)
+		} else {
+			bagcont := make([]Bag, 0, 2)
+			for _, c := range contents {
+				fmt.Println(bag, c)
+				curbag := Bag{"", 0}
+				m := bagre.FindStringSubmatch(c)
+				var err error
+				curbag.count, err = strconv.Atoi(m[1])
+				if err != nil {
+					panic(err)
+				}
+				curbag.name = m[2]
+				bagcont = append(bagcont, curbag)
 			}
-			curbag.name = m[2]
-			bagcont = append(bagcont, curbag)
+			bags[bag] = bagcont
 		}
-		bags[bag] = bagcont
 	}
 	if scerr := scanner.Err(); scerr != nil {
 		panic(scerr)
 	}
 	fmt.Printf("%d bags\n", len(bags))
+	fmt.Println("One Shiny Gold Bag Contains:", numbags(bags), "bags")
+}
+
+func numbags(bags BagMap) int {
+	totalbag := make(map[string]int)
+	return getnumbags(bags, totalbag, "shiny gold")-1
+}
+
+func getnumbags(bag BagMap, totalbag map[string]int, b string) int {
+	if v, ok := totalbag[b]; ok {
+		return v
+	}
+	totalbag[b] = 1
+	for _, ib := range bag[b] {
+		totalbag[b] += ib.count*getnumbags(bag, totalbag, ib.name)
+	}
+	return totalbag[b]
+}
+
+func printWays(bags BagMap) {
 	ways := make(map[string]int)
-	ways["shiny gold"] = 1
-	// We don't want use shiny gold directly
 	total := 0
 	for k, _ := range bags {
 		r := getways(bags, ways, k)
@@ -62,7 +82,7 @@ func main() {
 	fmt.Println(total)
 }
 
-func getways(bags map[string][]Bag, ways map[string]int, b string) int {
+func getways(bags BagMap, ways map[string]int, b string) int {
 	if v, ok := ways[b]; ok {
 		return v
 	}
