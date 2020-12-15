e6fe816e2f403d1175715b9cc1254e5dc4c4ac4f
Jacob Adams <jacob@tookmund.com>
2020-12-15 16:27:58 -0500

    day 15 part 2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,14 +1,15 @@
+use std::collections::hash_map::Entry::Occupied;
+use std::collections::hash_map::Entry::Vacant;
+use std::collections::HashMap;
 use std::io;
 use std::io::prelude::*;
-use std::collections::HashMap;
-use std::collections::hash_map::Entry::Vacant;
-use std::collections::hash_map::Entry::Occupied;
 
 #[derive(Debug)]
 struct Num {
     times: i32,
     when: i32,
 }
+const MAXTURN: i32 = 30000000;
 
 fn main() -> io::Result<()> {
     for line in io::stdin().lock().lines() {
@@ -21,44 +22,44 @@ fn main() -> io::Result<()> {
                 Ok(v) => v,
                 Err(e) => panic!("Failed to parse ({}) into int: {}", n, e),
             };
-            nums.insert(s, Num{
-                times: 1,
-                when: turn,
-            });
+            nums.insert(
+                s,
+                Num {
+                    times: 1,
+                    when: turn,
+                },
+            );
             //println!("Turn {}: {}", turn, s);
             turn += 1;
             last = s;
         }
         let mut say = 0;
-        for i in turn..=2020 {
+        for i in turn..=MAXTURN {
             match nums.entry(last) {
                 Vacant(v) => panic!("{} missing from nums?", v.key()),
                 Occupied(mut o) => {
                     let mut v = o.get_mut();
                     if v.times != 1 {
-                        say = (i-1)-v.when;
+                        say = (i - 1) - v.when;
                     } else {
                         say = 0;
                     }
-                    v.when = i-1;
-                },
+                    v.when = i - 1;
+                }
             }
             match nums.entry(say) {
                 Vacant(v) => {
-                    v.insert(Num {
-                        times: 1,
-                        when: i,
-                    });
-                },
+                    v.insert(Num { times: 1, when: i });
+                }
                 Occupied(mut o) => {
                     let mut v = o.get_mut();
                     v.times += 1;
-                },
+                }
             }
             //println!("Turn {}: {}", i, say);
             last = say;
         }
-        println!("{} Turn 2020: {}", l, say);
+        println!("{} Turn {}: {}", l, MAXTURN, say);
     }
 
     Ok(())
