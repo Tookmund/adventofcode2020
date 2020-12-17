bbd51d18e4e6a7b3c0554e98d5b4f1cc86f29950
Jacob Adams <jacob@tookmund.com>
2020-12-17 16:55:42 -0500

    rustfmt day 17

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,6 +1,6 @@
+use std::collections::HashSet;
 use std::io;
 use std::io::prelude::*;
-use std::collections::HashSet;
 
 type Coord = (i64, i64, i64, i64);
 type ConwayCubes = HashSet<Coord>;
@@ -14,24 +14,23 @@ fn main() -> io::Result<()> {
     let mut x = 0;
     let mut y = 0;
 
-
     for line in io::stdin().lock().lines() {
         y = 0;
         for c in line?.chars() {
             match c {
                 '#' => {
-                    map.insert((x,y,0,0));
+                    map.insert((x, y, 0, 0));
                     ()
-                },
+                }
                 '.' => (),
-                _ =>  panic!("Invalid char ({}) in input!", c),
+                _ => panic!("Invalid char ({}) in input!", c),
             }
-           y += 1;
+            y += 1;
         }
         x += 1;
     }
-    max.0 = x+CHANGE;
-    max.1 = y+CHANGE;
+    max.0 = x + CHANGE;
+    max.1 = y + CHANGE;
 
     for _ in 0..6 {
         let mut newmap = map.clone();
@@ -40,11 +39,10 @@ fn main() -> io::Result<()> {
                 for z in min.2..=max.2 {
                     for w in min.3..=max.3 {
                         let neighbors = get_neighbors(&map, &(x, y, z, w));
-                        let alive = map.contains(&(x,y,z,w));
+                        let alive = map.contains(&(x, y, z, w));
                         if !alive && neighbors == 3 {
                             newmap.insert((x, y, z, w));
-                        }
-                        else if alive && !(neighbors == 2 || neighbors == 3) {
+                        } else if alive && !(neighbors == 2 || neighbors == 3) {
                             newmap.remove(&(x, y, z, w));
                         }
                     }
@@ -68,11 +66,11 @@ fn main() -> io::Result<()> {
 
 fn get_neighbors(map: &ConwayCubes, c: &Coord) -> i32 {
     let mut ret = 0;
-    for x in c.0-1..=c.0+1 {
-        for y in c.1-1..=c.1+1 {
-            for z in c.2-1..=c.2+1 {
-                for w in c.3-1..=c.3+1 {
-                    if *c != (x,y,z,w) && map.contains(&(x, y, z, w)) {
+    for x in c.0 - 1..=c.0 + 1 {
+        for y in c.1 - 1..=c.1 + 1 {
+            for z in c.2 - 1..=c.2 + 1 {
+                for w in c.3 - 1..=c.3 + 1 {
+                    if *c != (x, y, z, w) && map.contains(&(x, y, z, w)) {
                         ret += 1;
                     }
                 }
